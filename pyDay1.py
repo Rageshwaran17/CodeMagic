74f4828682c46b3a8fe533c065478dd63637ff4e
Rageshwaran <126971620+Rageshwaran17@users.noreply.github.com>
2025-03-10 19:47:07 +0530

    Update pyDay1.py

--- pyDay1.py
+++ pyDay1.py
@@ -3,10 +3,15 @@ print("Vanakkam")
 name = input("Enter your name: ")
 print(len(name))
 '''
-
+'''
 #Band_name_generator
 print("Welcome to the Band Name Generator.")
 city_name = input("What's the name of the city you grew up in?\n")
 pet_name = input("What's your pet's name?\n")
 band_name = city_name +" "+pet_name
 print("Your band name could be " + band_name)
+'''
+print(type(1))
+print(type("a"))
+print(type(0.1))
+print(type(True))
